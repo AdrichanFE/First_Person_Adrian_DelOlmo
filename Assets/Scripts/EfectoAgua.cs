1a656a8a1a98fc58c725f773575c9cd3de55fbcf
AdrichanFE <adrielpelao_xty@hotmail.com>
2024-12-26 16:54:27 +0100

    Revert "cosas"
    This reverts commit 010e13f2a011863fc931baccc6328f289d95a611.

--- Assets/Scripts/EfectoAgua.cs
+++ Assets/Scripts/EfectoAgua.cs
@@ -6,7 +6,6 @@ using UnityEngine.Rendering.Universal;
 
 public class EfectoAgua : MonoBehaviour
 {
-    [SerializeField] private float velocidad;
     private Volume efecto;
     // Start is called before the first frame update
     void Start()
@@ -17,17 +16,9 @@ public class EfectoAgua : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
-        //Debug.Log(Mathf.Cos(velocidad*Time.time));
-
-
         if(efecto.profile.TryGet(out LensDistortion distorsion))
         {
-            FloatParameter xValue = new FloatParameter(1+Mathf.Cos(velocidad * Time.time)/2);
-            FloatParameter yValue = new FloatParameter(1+Mathf.Sin(velocidad * Time.time)/2);
-            distorsion.xMultiplier.SetValue(xValue);
-            distorsion.yMultiplier.SetValue(yValue);
+            //distorsion.xMultiplier
         }
         
     }
