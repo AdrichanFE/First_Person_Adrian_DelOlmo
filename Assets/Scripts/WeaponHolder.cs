b9c235cadda57fb36502c541dabd9af825fdc7d9
AdrichanFE <adrielpelao_xty@hotmail.com>
2025-01-03 19:37:51 +0100

    a

--- Assets/Scripts/WeaponHolder.cs
+++ Assets/Scripts/WeaponHolder.cs
@@ -6,7 +6,7 @@ public class WeaponHolder : MonoBehaviour
 {
     [SerializeField] private GameObject[] armas;
 
-    int indiceArmaActual=0;
+    private int indiceArmaActual=0;
     // Start is called before the first frame update
     void Start()
     {
